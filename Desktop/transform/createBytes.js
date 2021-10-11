1551b6f8e170a80dd05b119e9fe9fb304ec5605c
Uwabis <uwabis@yahoo.com>
2021-10-11 15:23:18 +0100

    second commit

--- Desktop/transform/createBytes.js
+++ Desktop/transform/createBytes.js
@@ -7,3 +7,15 @@ async function createBytes(args){
 }
 
 createBytes(process.argv.slice(2)); 
+
+
+/*to run command, open terminal and run; node createBytes.js <stringname>, this 
+will give u the convertion in Bytes...e.g: node createBytes.js paul.usembo
+to convert the Bytes back to strings; run node parseBtyes.js <Unicode> on the terminal. 
+
+but first you need to icd into the directory of the root folder, then nitialize npm: npm init --y 
+npm is a nodes libary manager which allows you to install libraries. 
+This helps bring in a libarary called ehters (search npm Ethers for more details on this)
+Then run npm i ethers. 
+*/
+
